1c3c5b631068e5a92ee9a755bde573b08165c276
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-10-16 11:44:53 +0200

    improve test

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,8 @@
 import { createStandardAction } from '../index';
 
-test('My Greeter', () => {
+test('createStandardAction should create action creator that creates... an action xD', () => {
     const ac = createStandardAction('START')<string>();
     expect(ac.getType()).toBe('START');
     expect(ac.toString()).toBe('START');
+    expect(ac("Hello")).toEqual({ type: "START", payload: "Hello", meta: undefined })
 });
