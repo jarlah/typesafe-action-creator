847611d67a34215fb0023e1cedaa9f6b6ea1d240
Jarl André Hübenthal <jarlah@users.noreply.github.com>
2019-10-16 12:48:08 +0200

    1.0.3

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,8 @@
 import { createStandardAction } from '../index';
 
 test('createStandardAction should create action creator that creates... an action xD', () => {
-    const ac = createStandardAction('START')<string>();
-    expect(ac.getType()).toBe('START');
-    expect(ac.toString()).toBe('START');
-    expect(ac("Hello")).toEqual({ type: "START", payload: "Hello", meta: undefined })
+  const ac = createStandardAction('START')<string>();
+  expect(ac.getType()).toBe('START');
+  expect(ac.toString()).toBe('START');
+  expect(ac('Hello')).toEqual({ type: 'START', payload: 'Hello', meta: undefined });
 });
